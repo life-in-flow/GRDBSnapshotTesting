3b1d04f4d5b23417f647d6da90d574eb7f4342fe
JJ Martin <martinjaco@gmail.com>
2025-10-16 01:31:08 -0400

    Updates GRDB dependency to sqlcipher-7.7.0
    Updates the GRDB dependency to the sqlcipher-7.7.0 branch.
    This ensures compatibility with the latest SQLCipher version.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["GRDBSnapshotTesting"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/life-in-flow/GRDB.swift", branch: "sqlcipher-package"),
+        .package(url: "https://github.com/life-in-flow/GRDB.swift", branch: "sqlcipher-7.7.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .upToNextMajor(from: "1.18.1")),
     ],
     targets: [
